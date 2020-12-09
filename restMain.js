01340a1ad18f2bd64f2e18272cdc8ec48ceba5a4
fusrohdart <nabyl0405@gmail.com>
2020-12-09 22:01:54 +0800

    restMain

--- restMain.js
+++ restMain.js
@@ -1,23 +1,37 @@
 //Imports 
 const express = require('express'); 
-import { auth } from './src/firebase'; 
+const firebase = require('firebase') 
 
 const restServer = express(); 
 
-restServer.get('/questions?SortBy=:type', (request, response) => { 
-    res.send(req.query.SortBy); 
+restServer.get('/questions', (request, response) => { 
+    response.send(request.query.SortBy); 
 });  
 
 // Sample Class for Posts
-export class post { 
-    constructor(userName, body, title) {
-        this._userName = userName; // auth.currentUser; 
-        this._body = body; 
+class post { 
+    constructor(userName, title, body, type, upCount, downCount) {
+        this._userName = userName; 
         this._title = title; 
-    }
+        this._body = body; 
+        this._type = type; 
+        this._upCount = upCount; 
+        this._downCount = downCount;
+    } 
+
+    get userName() { return this._userName; } 
+    get title() { return this._title; } 
+    get body() { return this._body; } 
+    get type() { return this._type; }
 }
 
-let clarkQ = new post('Clark', 'I have recently come across Javascript and I am having trouble understanding it', 'What is JavaScript?'); 
+let clarkQ = new post (
+    'Clark', 
+    'What is JavaScript?', 
+    'I have recently come across Javascript and I am having trouble understanding it', 
+    'q', 
+    4, 0 
+); 
 
 //Port Variable
 const port = process.env.PORT || 3000; 
