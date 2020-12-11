1f402f88d2b228729cc14e7b687e3206a488ad3b
fusrohdart <nabyl0405@gmail.com>
2020-12-11 16:47:57 +0800

    Update restMain.js

--- restMain.js
+++ restMain.js
@@ -12,7 +12,7 @@ const main = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'Dovahkiin@0405'
-  });
+});
 
 // Connect 
 main.connect((error) => {
@@ -22,30 +22,53 @@ main.connect((error) => {
     else {
         console.log('Connection successful!');
     }
-})
+});
 
-// Queries
+// Create database
 restServer.get('/database', (req, res) => {
-    let query = "";
+    let query = "SHOW DATABASES LIKE 'main'";
     main.query(query, (error, result) => {
-        
-    })
-    let query = "CREATE DATABASE main";
-        main.query(query, (error, result) => {
-            if (error) throw error; 
+        if (error) {
+            throw error;
+        } 
+        else if (result) {
             console.log(result);
-            res.send(`Query ${query} successful! Database created.`)
-        });
-})
+            res.send('Database main already exists.'); 
+        } 
+        else {
+            let query = "CREATE DATABASE main";
+            main.query(query, (error, result) => {
+                if (error) throw error; 
+                console.log(result);
+                res.send(`Query ${query} successful! Database created.`);
+            });
+        }
+    }); 
+});
 
+// Create questions table
 restServer.get('/database/questions', (req, res) => {
-    let query = "CREATE TABLE questions (question_ID INT AUTO_INCREMENT, title VARCHAR (175), body VARCHAR (255), vote_up_count SMALLINT, vote_down_count SMALLINT PRIMARY KEY (question_ID))";
+    let query = "SHOW TABLES FROM main LIKE 'questions'"; 
     main.query(query, (error, result) => {
-        if (error) throw error; 
-        console.log(result); 
-        res.send(`Query ${query} was successful! Questions table created.`); 
-    })
-})
+        if (error) {
+            throw error; 
+        } 
+        else if (result) {
+            console.log(result); 
+            let newQuestion = req.query.create;
+            console.log(newQuestion);
+            res.send(`Table questions already exists.`); 
+        } 
+        else {
+            let query = "CREATE TABLE questions (question_ID INT AUTO_INCREMENT, title VARCHAR (175), body VARCHAR (255), vote_up_count SMALLINT, vote_down_count SMALLINT PRIMARY KEY (question_ID))";
+            main.query(query, (error, result) => {
+                if (error) throw error; 
+                console.log(result); 
+                res.send(`Query ${query} was successful! Questions table created.`); 
+            });
+        }
+    });
+}); 
 
 // Port Variable
 const port = process.env.PORT || 3000; 
