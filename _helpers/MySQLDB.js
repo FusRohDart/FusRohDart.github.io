6c5f6fa17e8113caa4962118dd939c67878225e8
fusrohdart <nabyl0405@gmail.com>
2020-12-13 18:25:41 +0800

    Update MySQLDB.js

--- _helpers/MySQLDB.js
+++ _helpers/MySQLDB.js
@@ -1,16 +1,17 @@
-const mysql = require('mysql'); 
-const catchErrors = require('../_middleware/error-handler');
+const mysql = require('mysql');
 
 initializeDB();
 
 //Login to MySQL server
 async function initializeDB() {
-    const connectMySQL = await mysql.createConnection({
-        host     : process.env.MYSQL_DB_HOST || 'localhost',
-        user     : process.env.MYSQL_DB_USER || 'root',
-        password : process.env.MYSQL_DB_PASS || 'Dovahkiin@0405'
+    const connectMySQL = mysql.createConnection({
+        host: process.env.MYSQL_DB_HOST || 'localhost',
+        user: process.env.MYSQL_DB_USER || 'root',
+        password: process.env.MYSQL_DB_PASS || 'Dovahkiin@0405'
     });
     
-    await connectMySQL.query(`CREATE DATABASE IF NOT EXISTS ${process.env.MYSQL_DB_NAME};`);
-    await connectMySQL.query("CREATE TABLE IF NOT EXISTS questions");
+    connectMySQL.query(`CREATE DATABASE IF NOT EXISTS ${process.env.MYSQL_DB_NAME};`);
+    connectMySQL.query("CREATE TABLE IF NOT EXISTS questions");
+    connectMySQL.query("CREATE TABLE IF NOT EXISTS answers");
+    connectMySQL.query("CREATE TABLE IF NOT EXISTS comments");
 }
