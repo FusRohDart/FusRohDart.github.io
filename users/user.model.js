81705d94712d2da68b2178a4d59ddc417fe60b1e
fusrohdart <nabyl0405@gmail.com>
2020-12-14 22:54:19 +0800

    bcrypt

---
+++ users/user.model.js
@@ -0,0 +1,56 @@
+import { DataTypes } from 'sequelize';
+
+module.export = userModel;
+
+function userModel(sequelize) {
+    const elements = {
+        userID: {
+            allowNull: false,
+            primaryKey: true,
+            type: DataTypes.UUIDV4,
+            defaultValue: DataTypes.UUIDV4,
+            Validate: {
+                notNull: {
+                    msg: "Must not be null!"
+                }
+            }
+        },
+        userName: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        email: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        userTitle: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            DefaultValue: 'No title achieved yet.'
+        },
+        userDescription: {
+            allowNull: true,
+            type: DataTypes.STRING
+        },
+        totalPoints: {
+            allowNull: false,
+            type: DataTypes.SMALLINT,
+            defaultValue: 0
+        },
+        hash: {
+            allowNull: false,
+            type: DataTypes.STRING
+        }
+    }
+
+    const options = {
+        defaultScope: {
+            attributes: { exclude: ['hash'] }
+        },
+        scopes: {
+            withHash: { attributes: {}, }
+        }
+    };
+
+    return sequelize.define('User', elements, options);
+}
