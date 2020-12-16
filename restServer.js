92d73b57de167d6e6fa2c490b536de8c2df0f2d1
fusrohdart <nabyl0405@gmail.com>
2020-12-17 00:38:14 +0800

    rootpath

--- restServer.js
+++ restServer.js
@@ -1,3 +1,4 @@
+/*
 // Imports 
 import express from 'express';
 
@@ -9,3 +10,25 @@ const port = process.env.PORT || 3000;
 
 // Listen on Port
 restServer.listen(port, () => {console.log(`Listening on ${port}...`)});
+*/
+
+require('rootpath')(); 
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require.restServer.require('_middleware/error-handler.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
