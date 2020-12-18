76b203485c126244e0164153acd587a45a9ff692
fusrohdart <nabyl0405@gmail.com>
2020-12-19 01:41:42 +0800

    Update questions.controller.js

---
+++ questionPosts/questions.controller.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const validateRequest = require('_middleware/validate-request');
+const authorize = require('_middleware/authorize')
+const questionService = require('./questions.services');
